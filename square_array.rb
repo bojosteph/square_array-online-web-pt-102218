e5317d7e5989fbf61eb09a6e9abc38a2bfcee2f8
Roberto Samia <bojosteph@gmail.com>
2018-10-24 23:54:08 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,22 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |num|
+    new_array << num * num
+  end
+  new_array
 end
+
+
+numbers = [1,2,3]
+square_array(numbers) 
+
+new_numbers = [9,10,16,25]
+square_array(new_numbers)
+
+class Array
+  def square!
+    self.collect!{|num| num ** 2}
+  end
+end
+
+[1,2,3].square!  
